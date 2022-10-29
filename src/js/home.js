a43c001882c9df82278190b5c8724f9fbe7e1f00
Nathanael Gutierrez <nathanaelg16@gmail.com>
2022-10-29 13:07:53 -0400

    progress on assets page

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,7 @@
+import config from "../config.js";
+
 //let token = new URL(window.location.href).searchParams.get('token');
 let token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYXRoYW5hZWwiLCJpc3MiOiJQaHlzaWNzIEludmVudG9yeSBBdXRoZW50aWNhdGlvbiBTZXJ2aWNlIiwiaWF0IjoxNjY3MDU2MTc3LCJleHAiOjE2NjcwNTc5NzcsInJvbGVzIjoiIn0.TIAYL6KSR_ey0-xkTf7pZU10pR4gtqEhI09JrPyKdng";
 
+document.getElementById('logo').src = config["spaces"].concat("/logos/B_Physics Inventory-04.png");
+//document.getElementById('settings')
