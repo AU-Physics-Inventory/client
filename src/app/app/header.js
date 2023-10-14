42541a556f173b682b10cad08efa4020278d2e3d
Nathanael Gutierrez <nathanaelg16@gmail.com>
2023-10-14 11:20:00 -0400

    app layout

--- src/app/app/header.js
+++ src/app/app/header.js
@@ -1,3 +1,36 @@
-export default function Navbar() {
-    
+'use client'
+
+import Box from "@mui/joy/Box";
+import {AspectRatio, Sheet} from "@mui/joy";
+import Image from "next/image";
+import config from "@/resources/config";
+import Typography from "@mui/joy/Typography";
+
+export default function Header() {
+    return <>
+        <Sheet color="primary" variant="solid">
+            {/*<AspectRatio ratio="1" variant="plain">*/}
+            {/*    <Image src={config.spaces.concat("/logos/B_Physics Inventory-04.png")}*/}
+            {/*           alt="Physics Inventory" width="24" height="24"/>*/}
+            {/*</AspectRatio>*/}
+            {/*<Typography*/}
+            {/*    fontWeight="lg"*/}
+            {/*    fontSize="xl"*/}
+            {/*    startDecorator={<Box*/}
+            {/*        component="span"*/}
+            {/*        sx={{*/}
+            {/*            width: 48,*/}
+            {/*            height: 48,*/}
+            {/*        }}>*/}
+            {/*        <Image src={config.spaces.concat("/logos/B_Physics Inventory-04.png")}*/}
+            {/*               alt="Physics Inventory"*/}
+            {/*               width="48"*/}
+            {/*               height="48"*/}
+            {/*        />*/}
+            {/*    </Box>}*/}
+            {/*>*/}
+            {/*    Physics Inventory*/}
+            {/*</Typography>*/}
+        </Sheet>
+    </>
 }
