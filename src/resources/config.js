dbd5bf81100b18567616410f95511f21743b54f5
Nathanael Gutierrez <nathanaelg16@gmail.com>
2023-10-15 21:10:56 -0400

    mmodifying to get values from env variable

--- src/resources/config.js
+++ src/resources/config.js
@@ -1,6 +1,6 @@
 const appConfig = {
-    spaces: "https://khd5028k-dev.nyc3.cdn.digitaloceanspaces.com",
-    server: "http://192.168.1.218:8080"
+    spaces: process.env.NEXT_PUBLIC_SPACES,
+    server: process.env.NEXT_PUBLIC_SERVER
 };
 
 export default appConfig;
