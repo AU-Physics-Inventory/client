2b4cd28e7ceacb57725265bbd744b8205d2bce27
Nathanael Gutierrez <nathanaelg16@gmail.com>
2023-10-15 22:29:51 -0400

    hopefully this fixes static site generation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
+        unoptimized: true,
         domains: ['795h2gwn-dev.physicsinventory.gq', 'khd5028k-dev.nyc3.cdn.digitaloceanspaces.com']
     }
 }
