4e2a45e80db7309827a7b113eadbbd38c857eb48
Nathanael Gutierrez <nathanaelg16@gmail.com>
2023-10-23 20:20:56 -0400

    refactoring

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,16 @@
 import './globals.css'
-import { inter, roboto_slab} from "@/app/fonts";
-import Head from "next/head";
-import {CssVarsProvider, extendTheme} from "@mui/joy";
-import CssBaseline from "@mui/joy/CssBaseline";
-import Header from "@/app/(app)/header";
-import Box from "@mui/joy/Box";
-import Sidebar from "@/app/(app)/sidebar";
+import {roboto_slab} from "@/app/fonts";
 
 export const metadata = {
-  title: 'Physics Inventory',
-  description: 'AU Physics Inventory Management',
+    title: 'Physics Inventory',
+    description: 'AU Physics Inventory Management',
+    viewport: 'initial-scale=1, width=device-width'
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" className={roboto_slab.className}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <body>{children}</body>
+export default function RootLayout({children}) {
+    return <html lang="en" className={roboto_slab.className}>
+      <body>
+        {children}
+      </body>
     </html>
-  )
 }
