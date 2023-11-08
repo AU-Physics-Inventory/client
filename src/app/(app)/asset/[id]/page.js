7144661f4f667e5277872d0dc86043fa28a506d7
Nathanael Gutierrez <nathanaelg16@gmail.com>
2023-11-07 21:50:21 -0500

    initial work on asset display page

--- src/app/(app)/asset/[id]/page.js
+++ src/app/(app)/asset/[id]/page.js
@@ -7,6 +7,10 @@ import axios from "axios";
 import config from "@/resources/config";
 import Error from "@/app/(app)/error"
 import Box from "@mui/joy/Box";
+import Image from "next/image";
+import ImageNAImage from "@/resources/image_not_available.png";
+import {Card, Sheet} from "@mui/joy";
+import Button from "@mui/joy/Button";
 
 export default function Asset({params}) {
     const router = useRouter()
@@ -39,10 +43,31 @@ export default function Asset({params}) {
 
     return <>
         <Error error={error} clearError={() => setError(null)} />
-        <Box>
-            <Typography>
-                {asset.name}
-            </Typography>
+        <Box sx={{width: 1, height: 1, display: 'inline-block'}}>
+            <Sheet sx={{width: 1}}>
+                <Card sx={{height: '50vh', maxWidth: '800px', maxHeight: '600px', width: '80%', mx: 'auto', border: '2px solid blue'}} variant="plain">
+                            <Image alt="Item image"
+                                   src={asset.images == null ? ImageNAImage : config.spaces.concat('/images/').concat(asset.images[0])}
+                                   fill
+                                   sx={{objectFit: 'contain'}}
+                                   id={'image-' + asset.id}
+                            />
+                </Card>
+                <Box>
+                    <Typography level="title-lg" component="div">
+                        {asset.name}
+                    </Typography>
+                    <Typography level="body-lg">
+                        {asset.notes}
+                    </Typography>
+                    <Button variant="soft" color="primary" size="sm">
+                        Edit
+                    </Button>
+                    <Button variant="soft" color="danger" size="sm">
+                        Delete
+                    </Button>
+                </Box>
+            </Sheet>
         </Box>
     </>
 }
