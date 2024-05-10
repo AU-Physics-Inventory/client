6068b51bde520fad9059bb0a4e7f078b75535adc
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-05-10 09:40:47 -0400

    updates to next.js config to match updated version

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -4,8 +4,12 @@ import CssBaseline from "@mui/joy/CssBaseline";
 
 export const metadata = {
     title: 'Physics Inventory',
-    description: 'AU Physics Inventory Management',
-    viewport: 'initial-scale=1, width=device-width'
+    description: 'AU Physics Inventory Management'
+}
+
+export const viewport = {
+    initialScale: 1,
+    width: 'device-width'
 }
 
 export default function AppLayout({children}) {
